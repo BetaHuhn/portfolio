ffe8f740a02cf1d834719cf8292174647b76c3d1
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2020-09-26 17:15:10 +0200

    Add grid section

---
+++ src/components/experiences/Grid.vue
@@ -0,0 +1,57 @@
+<template>
+    <section class="image-grid">
+        <div class="content">
+            <h1>{{title}}</h1>
+            <p>{{subtitle}}</p>
+        </div>
+        <div class="images">
+            <img v-for="image in images" :key="image" :src="baseUrl + image" >
+        </div>
+    </section>
+</template>
+
+<script>
+    
+    export default {
+        name: 'Grid',
+        props: {
+            title: String,
+            subtitle: String,
+            images: Array,
+            baseUrl: String
+        }
+    }
+</script>
+
+<style scoped>
+    
+    .image-grid{
+        margin: auto;
+        margin-top: 8rem;
+        width: 90%;
+        text-align: center;
+    }
+
+    .content h1{
+        margin: 0;
+        font-size: 1.8rem;
+    }
+
+    .content p{
+        margin: 0;
+    }
+
+    .images {
+        margin-top: 2rem;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    img{
+        max-width: 30%;
+        margin: 10px;
+        box-shadow: 0 2px 50px 0 rgba(0, 0, 0, 0.18);
+    }
+
+</style>
