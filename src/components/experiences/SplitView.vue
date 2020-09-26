c0d39ffdd03a0f00f0342e033c6f2d917e0d0567
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2020-09-26 16:40:26 +0200

    Add split view section

---
+++ src/components/experiences/SplitView.vue
@@ -0,0 +1,58 @@
+<template>
+    <section class="split-view">
+        <div class="half left">
+            <img :src="image1.src">
+            <h3>{{image1.title}}</h3>
+        </div>
+        <div class="half right">
+            <img :src="image2.src">
+            <h3>{{image2.title}}</h3>
+        </div>
+    </section>
+</template>
+
+<script>
+    
+    export default {
+        name: 'SplitView',
+        props: {
+            image1: {
+                src: String,
+                title: String
+            },
+            image2: {
+                src: String,
+                title: String
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .split-view{
+        margin: auto;
+        margin-top: 8rem;
+        width: 80%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .half{
+        width: 50%;
+    }
+
+    .left img{
+        clip-path: polygon(0 0,100% 0,calc(100% - 50px) 100%,0 100%);
+    }
+
+    .right img{
+        clip-path: polygon(50px 0,100% 0,100% 100%,0 100%);
+    }
+
+    img {
+        width: 100%;
+    }
+
+
+</style>
